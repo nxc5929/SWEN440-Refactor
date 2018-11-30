b070ed831ac9c8f1e6bb8091e720f52045cfc794
Nathan Connor <nxc5929@rit.edu>
2018-11-30 16:58:07 -0500

    Initial commit

---
+++ src/main/java/org/rit/swen440/dataLayer/Category.java
@@ -0,0 +1,49 @@
+package org.rit.swen440.dataLayer;
+
+import lombok.Data;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+/**
+ * A product category supported by the Order System
+ */
+@Data
+public class Category {
+	private String name;
+	private String description;
+
+	private Set<Product> products = new HashSet<>();
+
+	public Optional<Product> findProduct(String name) {
+		return products.stream()
+				.filter(p -> p.getTitle().equalsIgnoreCase(name))
+				.findFirst();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Set<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(Set<Product> products) {
+		this.products = products;
+	}
+
+}
---
+++ src/main/java/org/rit/swen440/dataLayer/Product.java
@@ -0,0 +1,129 @@
+package org.rit.swen440.dataLayer;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.nio.file.Path;
+
+/**
+ * A record of each product type
+ */
+@Data
+public class Product {
+	@Setter(AccessLevel.PRIVATE)
+	private boolean updated = false;
+
+	private Path path;
+
+	private int skuCode;
+	private int itemCount;
+	private int threshold;
+	private int reorderAmount;
+	private String title;
+	private String description;
+	private BigDecimal cost;
+
+	/**
+	 * Check to see if we have enough of this item for an order
+	 *
+	 * @param amount Number of items being ordered
+	 * @return true if enough stock
+	 */
+	public boolean canOrder(int amount) {
+		return (itemCount - amount >= 0);
+	}
+
+	/**
+	 * Place an order, decrement the available itemCount
+	 *
+	 * @param amount being ordered
+	 * @return if order was successfully processed
+	 */
+	public boolean order(int amount) {
+		if (canOrder(amount)) {
+			itemCount = itemCount - amount;
+			setUpdated(true);  // Need to store the updated product information
+
+			// TODO:  add stock management functionality
+			return true;
+		}
+
+		return false;
+	}
+
+	private void setUpdated(boolean b) {
+		updated = b;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public Path getPath() {
+		return path;
+	}
+
+	public void setPath(Path path) {
+		this.path = path;
+	}
+
+	public int getSkuCode() {
+		return skuCode;
+	}
+
+	public void setSkuCode(int skuCode) {
+		this.skuCode = skuCode;
+	}
+
+	public int getItemCount() {
+		return itemCount;
+	}
+
+	public void setItemCount(int itemCount) {
+		this.itemCount = itemCount;
+	}
+
+	public int getThreshold() {
+		return threshold;
+	}
+
+	public void setThreshold(int threshold) {
+		this.threshold = threshold;
+	}
+
+	public int getReorderAmount() {
+		return reorderAmount;
+	}
+
+	public void setReorderAmount(int reorderAmount) {
+		this.reorderAmount = reorderAmount;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public BigDecimal getCost() {
+		return cost;
+	}
+
+	public void setCost(BigDecimal cost) {
+		this.cost = cost;
+	}
+
+	public boolean isUpdated() {
+		return updated;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+}
