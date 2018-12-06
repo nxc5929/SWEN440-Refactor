908f628a543fee9995edb239b3276882b196c1b5
Nathan Connor <nxc5929@rit.edu>
2018-12-06 15:33:15 -0500

    removed properties file

--- src/main/java/org/rit/swen440/menutest.java
+++ src/main/java/org/rit/swen440/menutest.java
@@ -7,23 +7,16 @@ import java.io.IOException;
 
 public class menutest
 {
-    public static void main(String[] args)
-    {
-        try {
-            System.getProperties().load(new FileInputStream("orderSys.properties"));
-            System.out.println("Hello");
-            menumgr mgr = new menumgr();
-            int currentLevel = 0;
-            boolean done = false;
-            do {
-                done = mgr.loadLevel(currentLevel);
-            } while (!done);
+	public static void main(String[] args){
+		System.out.println("Hello");
+		menumgr mgr = new menumgr();
+		int currentLevel = 0;
+		boolean done = false;
+		do {
+			done = mgr.loadLevel(currentLevel);
+		} while (!done);
 
-            System.out.println("Thank you for shopping at Hippolyta.com!");
+		System.out.println("Thank you for shopping at Hippolyta.com!");
 
-        } catch(IOException e) {
-            System.err.println("orderSys.properties not found.");
-        }
-
-    }
+	}
 }
