d7aeb8256517b04930c627eb6ee18953f6274d13
cjr8920 <crenn97@DESKTOP-QRLFJA8.localdomain>
2018-12-08 12:07:44 -0500

    Added additional tests, commented tests, and removed the old test

--- src/test/java/org/rit/swen440/menutestTest.java
+++ src/test/java/org/rit/swen440/menutestTest.java
@@ -1,10 +1,9 @@
 package org.rit.swen440;
 
 import static org.junit.Assert.assertTrue;
-
 import java.math.BigDecimal;
-
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 
 import org.junit.Test;
@@ -15,25 +14,26 @@ import org.rit.swen440.dataLayer.Product;
  */
 public class menutestTest
 {
+    //Create a new product for testing
 	private Product tester = new Product();
-	private BigDecimal price = new BigDecimal("3.99");
 
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
-    
+	//Test canOrder with a number that should return true
     @Test
     public void canOrderValidNumber()
     {
         tester.setItemCount(7);
     	assertTrue(tester.canOrder(5));
     }
-    
+
+    //Test canOrder with the same umber as the remaining stock
+    @Test
+    public void canOrderSameNumber()
+    {
+        tester.setItemCount(7);
+        assertTrue(tester.canOrder(7));
+    }
+
+    //Test canOrder with a number larger than the remaining stock
     @Test
     public void canOrderNonValidNumber()
     {
@@ -41,13 +41,15 @@ public class menutestTest
     	assertFalse(tester.canOrder(9));
     }
 
+    //Test canOrder with a negative number
 	@Test
 	public void canOrderNegativeNumber()
 	{
         tester.setItemCount(7);
 		assertFalse(tester.canOrder(-3));
 	}
-    
+
+	//Test order with a number smaller than the remaining stock
     @Test
     public void orderItemValidNumber()
     {
@@ -56,6 +58,16 @@ public class menutestTest
     	assertEquals(tester.getItemCount(), 4);
     }
 
+    //Test order with the same number as the remaining stock
+    @Test
+    public void orderItemSameNumber()
+    {
+        tester.setItemCount(7);
+        tester.order(7);
+        assertEquals(tester.getItemCount(), 0);
+    }
+
+    //Test order with a negative number
 	@Test
 	public void orderItemNegativeNumber()
 	{
@@ -64,6 +76,7 @@ public class menutestTest
 		assertEquals(tester.getItemCount(), 7);
 	}
 
+	//Test order with a number larger than the remaining stock
     @Test
     public void orderItemNonValidNumber()
     {
