699145a7e73aba39722535fb6dd1632adf9172e5
cjr8920 <crenn97@DESKTOP-QRLFJA8.localdomain>
2018-12-08 11:54:07 -0500

    Added unit tests for the command line functionality of ordering items and the order validity checker

--- src/test/java/org/rit/swen440/menutestTest.java
+++ src/test/java/org/rit/swen440/menutestTest.java
@@ -2,13 +2,22 @@ package org.rit.swen440;
 
 import static org.junit.Assert.assertTrue;
 
+import java.math.BigDecimal;
+
+import static org.junit.Assert.assertEquals;
+
+
 import org.junit.Test;
+import org.rit.swen440.dataLayer.Product;
 
 /**
  * Unit test for menutest Application.
  */
 public class menutestTest
 {
+	private Product tester = new Product();
+	private BigDecimal price = new BigDecimal("3.99");
+
     /**
      * Rigorous Test :-)
      */
@@ -16,5 +25,50 @@ public class menutestTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+    
+    @Test
+    public void canOrderValidNumber()
+    {
+        tester.setItemCount(7);
+    	assertTrue(tester.canOrder(5));
+    }
+    
+    @Test
+    public void canOrderNonValidNumber()
+    {
+        tester.setItemCount(7);
+    	assertFalse(tester.canOrder(9));
+    }
+
+	@Test
+	public void canOrderNegativeNumber()
+	{
+        tester.setItemCount(7);
+		assertFalse(tester.canOrder(-3));
+	}
+    
+    @Test
+    public void orderItemValidNumber()
+    {
+        tester.setItemCount(7);
+    	tester.order(3);
+    	assertEquals(tester.getItemCount(), 4);
+    }
+
+	@Test
+	public void orderItemNegativeNumber()
+	{
+        tester.setItemCount(7);
+		tester.order(-3);
+		assertEquals(tester.getItemCount(), 7);
+	}
+
+    @Test
+    public void orderItemNonValidNumber()
+    {
+        tester.setItemCount(7);
+    	tester.order(9);
+    	assertEquals(tester.getItemCount(), 7);
     }
 }
