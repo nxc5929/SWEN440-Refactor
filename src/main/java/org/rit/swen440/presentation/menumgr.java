2b63834ccb8049dae6b9090ed689609fdc0b30e6
KyleRohde <kylearohde@gmail.com>
2018-12-07 19:22:24 -0500

    Fixed menumgr Level0. Added method in menu to get menu size. Needed for Level0 fix.

--- src/main/java/org/rit/swen440/presentation/menumgr.java
+++ src/main/java/org/rit/swen440/presentation/menumgr.java
@@ -9,8 +9,7 @@ import java.util.Objects;
 import java.util.Optional;
 
 
-public class menumgr
-{
+public class menumgr {
     int currentLevel = 0;
     String currentCategoryName;
     String currentItemName;
@@ -18,8 +17,7 @@ public class menumgr
     item currentItem;
     private Controller controller;
 
-    public menumgr()
-    {
+    public menumgr() {
         controller = new Controller();
 
     }
@@ -30,11 +28,8 @@ public class menumgr
      * @param level
      * @return
      */
-    public boolean loadLevel(int level)
-    {
-//        System.out.println("Loading level:" + currentLevel);
-        switch (currentLevel)
-        {
+    public boolean loadLevel(int level) {
+        switch (currentLevel) {
             case -1:
                 return true;
             case 0:
@@ -56,32 +51,28 @@ public class menumgr
     /**
      * Asks user what category of product they would like to access
      */
-    public void Level0()
-    {
+    public void Level0() {
         menu m = new menu();
         List<String> categories = controller.getCategories();
         m.loadMenu(categories);
-        m.addMenuItem("'q' to Quit"); 
+        m.addMenuItem("'q' to Quit");
         System.out.println("The following org.rit.swen440.presentation.categories are available");
         m.printMenu();
-        String result = "0";
-        try
-        {
+        String result = "q";
+        int iSel = -1;
+
+        try {
             result = m.getSelection();
-        }
-        catch (Exception e)
-        {
+            iSel = Integer.parseInt(result);
+        } catch (Exception e) {
             result = "q";
         }
-        if (Objects.equals(result,"q"))
-        {
-            currentLevel--;
-        }
-        else
-        {
-            currentLevel++;
-            int iSel = Integer.parseInt(result);
 
+        if (result.equals("q") || iSel < 0 ||
+                iSel >= m.getMenuSize()-1) { //Must add -1 to include the added option 'q to Quit'
+            currentLevel--;
+        } else {
+            currentLevel++;
             currentCategoryName = categories.get(iSel);
             System.out.println("\nYour Selection was:" + currentCategoryName);
         }
@@ -90,8 +81,7 @@ public class menumgr
     /**
      * Asks user what specific product they would like to purchase from their selected category.
      */
-    public void Level1()
-    {
+    public void Level1() {
         menu m = new menu();
 
         //items it = new items("orderSys/" + currentCategory.getName());
@@ -100,31 +90,27 @@ public class menumgr
         List<String> itemList = controller.getProducts(currentCategoryName);
         List<String> l = new ArrayList<>();
         System.out.println("");
-        for (String itm: itemList)
+        for (String itm : itemList)
             l.add(controller.getProductInformation(currentCategoryName, itm, Controller.PRODUCT_FIELD.NAME)
-             + "($" + controller.getProductInformation(currentCategoryName, itm, Controller.PRODUCT_FIELD.COST) + ")");
-        
+                    + "($" + controller.getProductInformation(currentCategoryName, itm, Controller.PRODUCT_FIELD.COST) + ")");
+
         m.loadMenu(l);
         m.addMenuItem("'q' to quit");
         System.out.println("The following items are available");
         m.printMenu();
         String result = m.getSelection();
-        try
-        {
+        try {
             int iSel = Integer.parseInt(result);//Item  selected
             currentItemName = itemList.get(iSel);
             //currentItem = itemList.get(iSel);
             //Now read the file and print the org.rit.swen440.presentation.items in the catalog
             System.out.println("You want item from the catalog: " + currentItemName);
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             result = "q";
         }
         if (result == "q")
             currentLevel--;
-        else
-        {
+        else {
             //currentLevel++;//Or keep at same level?
             OrderQty(currentCategoryName, currentItemName);
         }
@@ -132,11 +118,11 @@ public class menumgr
 
     /**
      * Completes the purchase transaction
+     *
      * @param category
      * @param item
      */
-    public void OrderQty(String category, String item)
-    {
+    public void OrderQty(String category, String item) {
         System.out.println("Please select a quantity");
         System.out.println(controller.getProductInformation(category, item, Controller.PRODUCT_FIELD.NAME) +
                 " availability:" + controller.getProductInformation(category, item, Controller.PRODUCT_FIELD.INVENTORY));
